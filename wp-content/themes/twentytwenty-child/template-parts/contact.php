8b8ed3ed4a689207a2566b2892ceef19d2aa3f7c
flyplay91 <flyplay91@gmail.com>
2020-05-24 17:01:52 -0400

    create contact page

--- wp-content/themes/twentytwenty-child/template-parts/contact.php
+++ wp-content/themes/twentytwenty-child/template-parts/contact.php
@@ -12,25 +12,34 @@ get_header();
 ?>
 
 <main class="contact-page">
-    <div class="contact-page-hero">
-        <div class="contact-hero-left">
-            <img class="right-halfframe" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/right_halfframe_white.png">
-            <h2>let's<br><b>talk</b></h2>
-            <img class="left-halfframe" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/left_halfframe_white.png">
-            <img class="hero-dots" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/footer_dots.png">
-        </div>
-        <div class="contact-hero-right">
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/contact_hero_bg.png">
-        </div>
-    </div>
-    <div class="contact-page-form">
-        <div class="contact-form">
-            <?php echo do_shortcode( '[contact-form-7 id="212858" title="Contact Form"]' ); ?>
-        </div>
-        <div class="contact-form-dots">
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/footer_dots.png">
-        </div>
-    </div>
+    <?php if( have_rows('contact_group') ): ?>
+        <?php while( have_rows('contact_group') ): the_row();
+        $right_half_image = get_sub_field('right_half_image');
+        $hero_dot_image = get_sub_field('hero_dot_image');
+        $hero_image = get_sub_field('hero_image');
+        $left_half_image = get_sub_field('left_half_image');
+        $footer_dot_image = get_sub_field('form_dot_image'); ?>
+            <div class="contact-page-hero">
+                <div class="contact-hero-left">
+                    <img class="right-halfframe" src="<?php echo esc_url( $right_half_image['url'] ); ?>">
+                    <h2><?php echo get_sub_field('hero_title'); ?></h2>
+                    <img class="left-halfframe" src="<?php echo esc_url( $left_half_image['url'] ); ?>">
+                    <img class="hero-dots" src="<?php echo esc_url( $hero_dot_image['url'] ); ?>">
+                </div>
+                <div class="contact-hero-right">
+                    <img src="<?php echo esc_url( $hero_image['url'] ); ?>">
+                </div>
+            </div>
+            <div class="contact-page-form">
+                <div class="contact-form">
+                    <?php echo do_shortcode( get_sub_field('form_shortcode') ); ?>
+                </div>
+                <div class="contact-form-dots">
+                    <img src="<?php echo esc_url( $footer_dot_image['url'] ); ?>">
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
