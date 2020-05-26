16e950d74759775c123bc8180c7f6d214a9b9b58
flyplay91 <flyplay91@gmail.com>
2020-05-26 11:55:46 -0400

    create spectruss MD page

---
+++ wp-content/themes/twentytwenty-child/template-parts/spectruss-md.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Template Name: MD Template
+ * Template Post Type: page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+get_header();
+?>
+
+<main class="spectruss-md-page">
+    <div class="spectruss-md-intro">
+        <div class="md-intro-video">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/md_hero.jpg">
+        </div>
+        <div class="md-intro-heading-title-container">
+            <img class="md-mark" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/spectruss-md-heading-bg.png">
+            <img class="spectruss-md-mark" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/spectruss-md-title.png">
+        </div>
+        <div class="md-intro-paragraph-section">
+            <h2>It's time to switch</h2>
+            <p>Our Spectruss MD packages are built for businesses who are looking to build a strong, cohesive brand identity, while also saving money by eliminating mark-up from unnecessary 3rd party vendors. Everything you need, under one roof.</p>
+        </div>
+    </div>
+    <div class="spectruss-md-items">
+        <div class="spectruss-md-item-1">
+            <div class="md-item-1-left">
+                <p class="num">01.1</span>
+                <p class="item-title-1">What it looks like working with an</p>
+                <h3 class="item-subtitle-1">Agency of Record</h3>
+                <hr class="item-subtitle-underline">
+                <h2 class="model-text-1">Model</h2>
+                <div class="paragraph">
+                    <p>The typical Agency of Record model worked great until the agencies refused to grow with the demand for digital marketing and advertising. This meant that the typical AOR was outsourcing all of their technical deliverables such as PPC, Programming, Email Campaigns, Data and Analytics, Video / Photo, Social Management, Design, and PR. This not only costs your company more money due to additional mark-up but also weakens your brand identity due to the lack of consistency.</p>
+                </div>
+            </div>
+            <div class="md-item-1-right">
+                <img class="item-1-right-img" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/team-of-specialists.png">
+                <img class="item-1-right-img-half" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/vcmo_contract_right_half.png">
+                <img class="item-1-right-img-dot" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/vcmo_work_top_dot.png">
+            </div>
+        </div>
+        <div class="spectruss-md-item-2">
+            <div class="md-item-2-left">
+                <p class="num">01.2</span>
+                <p class="item-title-2">What it looks like hiring in house</p>
+                <h3 class="item-subtitle-2">team of specialists</p>
+                <hr class="item-subtitle-underline">
+                <h2 class="model-text-2">Model</h2>
+                <div class="paragraph">
+                    <p>A lot of companies have moved to the model of hiring multiple specialist vendors that supply everything directly to them. This was mainly done because the old school AOR became an unnecessary middle man that was too greedy. In a lot of ways this makes sense, however, there are still flaws with this system. With the additional 3rd party vendors communicating directly with your business, you have to staff full-time employees to direct and manage each of these vendors. On top of that, each vendor will have a margin that is higher due to them only working on one small part of the business. The most important flaw is the lack of consistency in brand identity. With too many chefs in the kitchen, the brand weakens and time to live is dramatically slower.</p>
+                </div>
+            </div>
+            <div class="md-item-2-right">
+                <img class="item-2-right-img" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/team-of-specialists.png">
+                <img class="item-2-right-img-dot" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/vcmo_work_top_dot.png">
+            </div>
+        </div>
+    </div>
+    <div class="spectruss-md-model-1">
+        <div class="black-bar"></div>
+        <div class="white-bar"></div>
+        <div class="md-model-1-content">
+            <div class="smd-top-row">
+                <p class="subtitle">The</p>
+                <p class="title">Spectruss</p>
+                <hr class="title-underline">
+                <p class="model-text">MODEL</p>
+            </div>
+            <div class="smd-middle-row">
+                <p class="left-paragraph">At Spectruss, we have built a team that is capable of managing and executing all forms of marketing and advertising in house. This not only strengthens your brand identity and decreases your time-to-live for projects, but also lowers your overall expense. Spectruss MD is the cutting edge choice for companies looking to grow their brand.</p>
+                <p class="right-paragraph">Tell us what you want, and we’ll make it happen! Spectruss MD gives your company access to our powerhouse of skills including design, web development, digital media, photography, videography, PR, and more. Our team will work with your monthly goals to make all your marketing ideas come to life!</p>
+            </div>
+            <div class="smd-bottom-row">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/spectruss-model-diagram.png">
+            </div>
+        </div>
+    </div>
+    <div class="spectruss-md-model-2">
+        <div class="heading-row">
+            <img class="heading-row-left" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/spectruss-levels-image.png">
+            <img class="heading-row-right" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/spectruss-levels-office-image.jpg">
+        </div>
+        <div class="levels-section">
+            <div class="level-block level-block--1">
+                <div class="level-block-left">
+                    <div class="level-top-row">
+                        <div class="level-hours-box">
+                            <p class="level-hours-text">40</p>
+                            <p class="hours-per-month">hrs/mo</p>
+                        </div>
+                        <p class="level-title">Entry<br>Level</p>
+                    </div>
+                    <div class="level-bottom-row">
+                        <p class="level-paragraph">The Entry Level Spectruss MD is perfect for those businesses looking to have someone manage smaller budget digital advertising campaigns, manage social media accounts, and send one to two monthly email campaigns. In reality, this is the beginning of your marketing strategy, as 40 hours can be used very quickly in a month.</p>
+                    </div>
+                </div>
+                <div class="level-block-right">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/entry-level-chart.png">
+                </div>
+            </div>
+            <div class="level-block level-block--2">
+                <div class="level-block-left">
+                    <div class="level-top-row">
+                        <div class="level-hours-box">
+                            <p class="level-hours-text">80</p>
+                            <p class="hours-per-month">hrs/mo</p>
+                        </div>
+                        <p class="level-title">silver<br>Level</p>
+                    </div>
+                    <div class="level-bottom-row">
+                        <p class="level-paragraph">The Silver Level Spectruss MD opens up our ability to manage larger advertising budgets across more platforms, increase our engagement on social media, take on small programming tasks, and start building a content library for campaigns. The Silver Level package is great for those who are still limited on budget, but want to develop a more complete digital experience for their customers.</p>
+                    </div>
+                </div>
+                <div class="level-block-right">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/silver-level-chart.png">
+                </div>
+            </div>
+            <div class="level-block level-block--3">
+                <div class="level-block-left">
+                    <div class="level-top-row">
+                        <div class="level-hours-box">
+                            <p class="level-hours-text">160</p>
+                            <p class="hours-per-month">hrs/mo</p>
+                        </div>
+                        <p class="level-title">Gold<br>Level</p>
+                    </div>
+                    <div class="level-bottom-row">
+                        <p class="level-paragraph">The Gold Level Spectruss MD is where we really start to shine. At 160 hours per month, we are able to execute small campaigns that utilize photos, videos or designed animations created in house. We can then develop a strategy to deliver this content across digital platforms with highly targeted demographic settings to maximize conversions. Our programmers will have more time available to them to develop more cutting edge pages and focus on the small details that make a big difference in increasing conversion rates.</p>
+                    </div>
+                </div>
+                <div class="level-block-right">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/gold-level-chart.png">
+                </div>
+            </div>
+            <div class="level-block level-block--4">
+                <div class="level-block-left">
+                    <div class="level-top-row">
+                        <div class="level-hours-box">
+                            <p class="level-hours-text">220+</p>
+                            <p class="hours-per-month">hrs/mo</p>
+                        </div>
+                        <p class="level-title">Platinum<br>Level</p>
+                    </div>
+                    <div class="level-bottom-row">
+                        <p class="level-paragraph">Our Platinum Level Spectruss MD is our most popular package as it offers our lowest hourly rate. Clients that choose this package typically use Spectruss as their full-service marketing/advertising department. We have the ability to develop national- level campaigns, large cutting edge websites, and a complete advertising/marketing strategy across every network possible. Clients have seen as much as $10k per month saved by switching from multiple 3rd party companies to our Platinum Level package. All of our hours are tracked using digital time tracking software, and the client will be notified before we go over our hourly budget.</p>
+                    </div>
+                </div>
+                <div class="level-block-right">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/platinum-level-chart.png">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="spectruss-md-page-form">
+        <h2>like that?<br><b>hit us up!</b></h2>
+        <div class="spectruss-md-form-dots">
+            <div class="spectruss-md-form">
+                <?php echo do_shortcode('[contact-form-7 id="212855" title="Contact Form"]'); ?>
+            </div>
+            <div class="spectruss-md-dots">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/footer_dots.png">
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+get_footer();
+?>
